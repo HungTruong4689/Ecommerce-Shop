804b179b49984bcf8cdb32f7e0923da69517fad2
HungTruong4689 <hungphuc4689@gmail.com>
2022-05-24 22:03:39 +0200

    First app

---
+++ src/Subtotal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import {getBasketTotal} from './reducer';
+import {useStateValue} from "./StateProvider";
+const Subtotal = () => {
+    const [{basket},dispatch] = useStateValue();
+    return (
+        <div className="subtotal">
+            <CurrencyFormat 
+                renderText = {(value) =>(
+                <>
+                <p>
+                    Subtotal ({basket.length} items): <strong>${value}</strong>
+                    <small className="subtotal__gift">
+                        <input type="checkbox" /> This order contains a gift
+                    </small>
+                    </p>
+                </>  
+                )} 
+                decimalScale={2}
+                value={getBasketTotal(basket)} 
+                displayType={"text"}
+                thousandSeparator={true}
+                
+            />
+            <button>Proceed to Checkout</button>
+        </div>
+    );
+};
+
+export default Subtotal;
